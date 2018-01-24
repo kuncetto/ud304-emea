a3db7d9f5da4b81ff3ead4fb136f942b2e8cce2f
Concetto Condorelli <concetto.condorelli@live.com>
2018-01-24 11:28:53 +0100

    Make a grid of fixed size when user submits the form

--- projects/pixel-art-maker/designs.js
+++ projects/pixel-art-maker/designs.js
@@ -2,9 +2,28 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(e) {
+  e.preventDefault();
+  makeGrid();
+});
 
 function makeGrid() {
 
-// Your code goes here!
+  // Your code goes here!
+  var height = 10;
+  var width = 10;
+  var canvas = $('#pixel_canvas');
 
+  // clear pixel canvas
+  canvas.empty();
+
+  for (var i = 0; i < height; i++) {
+    var row = $('<tr></tr>');
+    for (var j = 0; j < width; j++) {
+      // add column to row
+      row.append($('<td></td>'));
+    }
+    // add row to table
+    canvas.append(row);
+  }
 }
