84985114654127161b705ef7afc6afcb62868ebd
Concetto Condorelli <concetto.condorelli@live.com>
2018-01-24 18:07:05 +0100

    Prevent JavaScript code from being executed when document is not completely loaded

--- projects/pixel-art-maker/designs.js
+++ projects/pixel-art-maker/designs.js
@@ -1,36 +1,37 @@
-// Select color input
-const colorPicker = $('#colorPicker');
+$(document).ready(function() {
+  // Select color input
+  const colorPicker = $('#colorPicker');
 
-// Select size input
-const inputHeight = $('#input_height');
-const inputWidth = $('#input_width');
+  // Select size input
+  const inputHeight = $('#input_height');
+  const inputWidth = $('#input_width');
 
-// When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function(e) {
-  e.preventDefault();
-  let height = inputHeight.val();
-  let width = inputWidth.val();
-  makeGrid(height, width);
-});
+  // When size is submitted by the user, call makeGrid()
+  $('#sizePicker').submit(function(e) {
+    e.preventDefault();
+    let height = inputHeight.val();
+    let width = inputWidth.val();
+    makeGrid(height, width);
+  });
 
-$('#pixel_canvas').on('click', 'td', function() {
-  $(this).css('background-color', colorPicker.val());
-});
+  $('#pixel_canvas').on('click', 'td', function() {
+    $(this).css('background-color', colorPicker.val());
+  });
 
-function makeGrid(height, width) {
+  function makeGrid(height, width) {
 
-  // Your code goes here!
-  const canvas = $('#pixel_canvas');
+    // Your code goes here!
+    const canvas = $('#pixel_canvas');
 
-  // clear pixel canvas
-  canvas.empty();
+    // clear pixel canvas
+    canvas.empty();
 
-  for (let i = 0; i < height; i++) {
-    $('<tr></tr>').appendTo(canvas);
+    for (let i = 0; i < height; i++) {
+      $('<tr></tr>').appendTo(canvas);
+    }
+
+    for (let j = 0; j < width; j++) {
+      $('<td></td>').appendTo('tr');
+    }
   }
-
-  for (let j = 0; j < width; j++) {
-    $('<td></td>').appendTo('tr');
-  }
-
-}
+});
