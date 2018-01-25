247e583c1d67fac156b39ba80e07a710e3a33003
Concetto Condorelli <concetto.condorelli@live.com>
2018-01-25 10:34:40 +0100

    feat: Clear grid on reset and clean a cell on double click

--- projects/pixel-art-maker/designs.js
+++ projects/pixel-art-maker/designs.js
@@ -6,32 +6,54 @@ $(document).ready(function() {
   const inputHeight = $('#input_height');
   const inputWidth = $('#input_width');
 
+  // Select size picker form
+  const sizePicker = $('#sizePicker');
+
+  // Select pixel canvas table
+  const pixelCanvas = $('#pixel_canvas');
+
   // When size is submitted by the user, call makeGrid()
-  $('#sizePicker').submit(function(e) {
+  sizePicker.submit(function(e) {
     e.preventDefault();
     let height = inputHeight.val();
     let width = inputWidth.val();
     makeGrid(height, width);
   });
 
-  $('#pixel_canvas').on('click', 'td', function() {
+  // When user reset the form, call resetGrid()
+  sizePicker.children('[type="reset"]').on('click', function(e) {
+    e.preventDefault();
+    resetGrid();
+  });
+
+  // Click on a cell to color it
+  pixelCanvas.on('click', 'td', function() {
     $(this).css('background-color', colorPicker.val());
   });
 
+  // Double click on a cell to clean it
+  pixelCanvas.on('dblclick', 'td', function() {
+    $(this).css('background-color', '');
+  })
+
   function makeGrid(height, width) {
 
-    // Your code goes here!
-    const canvas = $('#pixel_canvas');
+    resetGrid();
 
-    // clear pixel canvas
-    canvas.empty();
-
+    // add rows to table
     for (let i = 0; i < height; i++) {
-      $('<tr></tr>').appendTo(canvas);
+      $('<tr></tr>').appendTo(pixelCanvas);
     }
 
+    // add columns to table rows
     for (let j = 0; j < width; j++) {
       $('<td></td>').appendTo('tr');
     }
   }
+
+  function resetGrid() {
+    // clear pixel canvas table
+    pixelCanvas.empty();
+  }
+
 });
